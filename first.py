b7ee3ca8da88dfe7603ad1c5c30e3ec589ece262
alexowallace <ay98127634@gmail.com>
2024-08-12 22:15:07 +0545

    initial commit

--- first.py
+++ first.py
@@ -1,1 +1,51 @@
-print("hello world")
+# b = 3
+# b += 3
+# print(b)
+
+# b -= 3
+# print(b)
+
+# d = 4>5
+# print(d)
+
+# f = 5<5
+# print(f)
+
+# g = 8==8
+# print(g)
+
+
+# e = True or False
+# print(e)
+
+# e = True and False
+# print(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- var_data_types.py
+++ var_data_types.py
@@ -1,1 +1,13 @@
-print("ashok")
+from tkinter.tix import Tree
+
+
+a = 1
+b = "ashok"
+c = 20.3
+d = False
+f = True
+e = None
+print(a,b,c)
+
+
+ 
