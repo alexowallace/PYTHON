b190efa52655435c5c46718345054e722a45df1f
alexowallace <ay98127634@gmail.com>
2024-08-13 09:14:49 +0545

    initial commit

--- first.py
+++ first.py
@@ -1,122 +1,123 @@
-# b = 3
-# b += 3
-# print(b)
+b = 3
+b += 3
+print(b)
 
-# b -= 3
-# print(b)
+b -= 3
+print(b)
 
-# d = 4>5
-# print(d)
+d = 4>5
+print(d)
 
-# f = 5<5
-# print(f)
+f = 5<5
+print(f)
 
-# g = 8==8
-# print(g)
-
-
-# e = True or False
-# print(e)
-
-# e = True and False
-# print(e)
-
-# print(not(True))
-# print(not(False))
-
-# x = int(1) 
-# print(x)  
-# y = int(2.8)
-# print(y) 
-# z = int("3")
-# print(z  
-
-# x = float(1) 
-# print(x)      
-# y = float(2.8) 
-# print(y)    
-# z = float("3") 
-# print(z)    
-# w = float("4.2")
-# print(w    
+g = 8==8
+print(g)
 
 
+e = True or False
+print(e)
 
-# x = str("s1") 
-# print(x    
-# y = str(2)
-# print(y)      
-# z = str(3.0) 
-# print(z)     
+e = True and False
+print(e)
+
+print(not(True))
+print(not(False))
+
+x = int(1) 
+print(x)  
+y = int(2.8)
+print(y) 
+z = int("3")
+print(z  
+
+x = float(1) 
+print(x)      
+y = float(2.8) 
+print(y)    
+z = float("3") 
+print(z)    
+w = float("4.2")
+print(w    
 
 
-# a = "34"
-# b = float(a)
-# c = type(b)
-# print(c)
-# a = 56.443
-# b = int(a)
-# c = type(b)
-# print(c)
 
-# a = int(input("enter num 1 "))
-# b = int(input("enter num 2 "))
-# print("num 1 is: ",a)
-# print("num 2 is: ",b)
-# print("sum i:", a+b)
+x = str("s1") 
+print(x    
+y = str(2)
+print(y)      
+z = str(3.0) 
+print(z)     
 
-# a = 7
-# b = 2
-# print("remainder is ", a%b)
-# a = input("enter any value of a ")
-# print (type(a))
 
-# a = int(input("enter any value of a "))
-# b = int(input("enter any value of b "))
-# print("a is greater than b is", a>b)
-# print("the average of a and b is: ", (a+b)/2)
-# print("the square of num that you entered is ", a**2,b*b)
+a = "34"
+b = float(a)
+c = type(b)
+print(c)
+a = 56.443
+b = int(a)
+c = type(b)
+print(c)
+
+a = int(input("enter num 1 "))
+b = int(input("enter num 2 "))
+print("num 1 is: ",a)
+print("num 2 is: ",b)
+print("sum i:", a+b)
+
+a = 7
+b = 2
+print("remainder is ", a%b)
+a = input("enter any value of a ")
+print (type(a))
+
+a = int(input("enter any value of a "))
+b = int(input("enter any value of b "))
+print("a is greater than b is", a>b)
+print("the average of a and b is: ", (a+b)/2)
+print("the square of num that you entered is ", a**2,b*b)
 
 
 
 name = 'ashok'
-# namlen = len(name)
-# print(namlen)
-# slice = name[0:3]
-# print(slice)
-# print(name[0])
-# print(name[:3])
-# print(name[3:])
+namlen = len(name)
+print(namlen)
+slice = name[0:3]
+print(slice)
+print(name[0])
+print(name[:3])
+print(name[3:])
 
 
 num = "0123456789"
-# print(num[1:8:5])
+print(num[1:8:5])
 
 
 name = 'ashok'
-# print(len(num))
-# print(name.endswith("ok"))
-# print(name.startswith("as"))
-# print(name.capitalize())
-# print(name.find("h"))
-# print(name.replace("h" , "z"))
+print(len(num))
+print(name.endswith("ok"))
+print(name.startswith("as"))
+print(name.capitalize())
+print(name.find("h"))
+print(name.replace("h" , "z"))
 
 
 
-# a = "i am hero and \"villan\" too  and \n i am gus also"
-# print (a)
+a = "i am hero and \"villan\" too  and \n i am gus also"
+print (a)
 
-# nam = input("enter your name:")
-# print (f"good afternoon {nam} ")
+nam = input("enter your name:")
+print (f"good afternoon {nam} ")
 
-# letter = '''dear <|Name|>,
-# you are selected!
-# <|date|>'''
+letter = '''dear <|Name|>,
+you are selected!
+<|date|>'''
 
-# print(letter.replace("<|Name|>", "ashok").replace("<|date|>", "25 sept 2049"))
+print(letter.replace("<|Name|>", "ashok").replace("<|date|>", "25 sept 2049"))
 
-a = "i am   hero and \"villan\" too   and \n i am gus   also"
+a = "i am  hero and \"villan\" too  and \n i am gus  also"
 print(a.find("  "))
+print(a.replace("  "," "))
 
 
 
---
+++ list_tuples.py
@@ -0,0 +1,75 @@
+friends = ['ashok' ,'is' ,'very',' good boy ']
+print(friends)
+print(friends[2:3])
+
+friends[1] = "hello world"
+print(friends)
+friends.append("and girls like it ")
+print(friends)
+
+li = [1,5,7,9,6,42,1,2,56,]
+li.sort()
+li.reverse()
+li.insert(2,2332)
+li.pop(2)
+print(li)
+print(li.pop(2))
+li.remove(7)
+print(li)
+
+a = (1,2,3,4,5)
+b = (1,)
+
+print(a)
+print(type(a))
+print(type(b))
+repeated = a * 3
+print(repeated)
+
+no = a.count(3)
+index = a.index(3)
+print(no)
+print(index)
+
+concat = a +b
+print (concat)
+
+print(len(a) )
+
+fruit = []
+f1 = input("enter fruit name:")
+fruit.append(f1)
+f2 = input("enter fruit name:")
+fruit.append(f2)
+f3 = input("enter fruit name:")
+fruit.append(f3)
+f3 = input("enter fruit name:")
+fruit.append(f3)
+f4 = input("enter fruit name:")
+fruit.append(f4)
+print(fruit)
+
+l=[2,0,232,454,0,34,0,2342,0,34,56,76,23]
+count = l.count(0)
+print(count)
+print(sum(l))
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
