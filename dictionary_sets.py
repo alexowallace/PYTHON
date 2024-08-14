12cfd803c52c428e7e36bbbe0d77fd489dac543a
alexowallace <ay98127634@gmail.com>
2024-08-14 07:52:46 +0545

    initial commit

---
+++ dictionary_sets.py
@@ -0,0 +1,30 @@
+marks = {
+    "harry" : 100,
+    "ashok" : 99,
+    "rohan" : 34,
+    0 :70
+}
+
+print(marks , type(marks))
+# print(marks["ashok"])
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"harry":80, "ashuka" :75})
+# print(marks)
+
+# print(marks.get("harry"))
+# a = marks.clear()
+# print(a)
+# print(marks , type(marks))
+
+# a = marks.pop("ashok",'99')
+# print(a)
+# print(marks , type(marks))
+
+# a = marks.popitem()
+# print(a)
+# print(marks , type(marks))
+
+
+
